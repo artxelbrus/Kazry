3e4476e0383df075ee553615fab802ab27dee6b3
artxelbrus <artxelbrus@gmail.com>
2026-08-19 23:51:09 +0300

    Update __init__.py

--- kazry/__init__.py
+++ kazry/__init__.py
@@ -1,3 +1,3 @@
-from .activation import KazryWithBackward, KazryInPlace, kazry_with_backward, kazry_inplace
+from .activation import KazryWithBackward, KazryInPlace, kazry_with_backward, kazry_inplace, KazryPyTorch, kazry_pytorch
 
 __all__ = ["KazryWithBackward", "KazryInPlace", "kazry_with_backward", "kazry_inplace", "KazryPyTorch", "kazry_pytorch"]
